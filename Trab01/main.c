e2224b670bec3b8e3db4fc718f04bec132d832d3
gdsantana <guilherme30santana@gmail.com>
2023-06-01 19:26:05 -0300

    Etapa 1 Finalizada

--- Trab01/main.c
+++ Trab01/main.c
@@ -1,56 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-//lex.yy.h
-int yylex();
-extern char *yytext;
-extern FILE *yyin;
-
-
-int isRunning(void);
-void initMe(void);
-
-int main(int argc, char** argv)
-  {
-  FILE *gold = 0;
-  int token = 0;
-  int answar = 0;
-  int nota = 0;
-  int i=1;
-      fprintf(stderr,"Rodando main do prof. \n");
-
-  if (argc < 3)
-    {
-    printf("call: ./etapa1 input.txt output.txt \n");
-    exit(1);
-    }
-  if (0==(yyin = fopen(argv[1],"r")))
-    {
-    printf("Cannot open file %s... \n",argv[1]);
-    exit(1);
-    }
-  if (0==(gold = fopen(argv[2],"r")))
-    {
-    printf("Cannot open file %s... \n",argv[2]);
-    exit(1);
-    }
-  initMe();
-  while (isRunning())
-    {
-    token = yylex();
-    
-    if (!isRunning())
-      break;
-    fscanf(gold,"%d",&answar);
-    if (token == answar)
-      {
-      fprintf(stderr,"%d=ok(%s)  ",i,yytext  );
-      ++nota;
-      }
-    else
-      fprintf(stderr,"\n%d=ERROR(%s,%d,%d) ",i,yytext,token,answar );
-    ++i;
-    }
-  printf("NOTA %d\n\n",nota);  
-  fprintf(stderr,"NOTA %d\n\n",nota);  
+int main(int argc, char** argv) {
+  
+  // se existe um arquivo lê ele
+  // se não lê do console;
+  if (argc > 1) { 
+    yyin = fopen(argv[1], "r");
   }
+
+  hashInit();
+
+  int token = yylex();
+
+  while(isrunning) {
+    switch(token){
+      case KW_CHAR:             printf("KW_CHAR '%d'\n", token);            break;      
+      case KW_INT:              printf("KW_INT '%d'\n", token);             break; 
+      case KW_REAL:              printf("KW_REAL '%d'\n", token);           break; 
+
+      case KW_IF:               printf("KW_IF '%d'\n", token);              break; 
+      case KW_THEN:             printf("KW_THEN '%d'\n", token);            break;   
+      case KW_ELSE:             printf("KW_ELSE '%d'\n", token);            break; 
+      case KW_LOOP:             printf("KW_LOOP '%d'\n", token);             break; 
+      case KW_INPUT:             printf("KW_INPUT '%d'\n", token);          break;
+      case KW_OUTPUT:             printf("KW_OUTPUT '%d'\n", token);        break;
+      case KW_RETURN:           printf("KW_RETURN '%d'\n", token);          break;  
+
+      case OPERATOR_LE:         printf("OPERATOR_LE '%d'\n", token);        break;
+      case OPERATOR_GE:         printf("OPERATOR_GE '%d'\n", token);        break;
+      case OPERATOR_EQ:         printf("OPERATOR_EQ '%d'\n", token);        break; 
+      case OPERATOR_DIF:        printf("OPERATOR_DIF '%d'\n", token);       break;  
+
+      case TK_IDENTIFIER:       printf("TK_IDENTIFIER '%d'\n", token);      break;  
+
+      case LIT_INT:             printf("LIT_INT '%d'\n", token);         break;  
+      case LIT_REAL:             printf("LIT_REAL '%d'\n", token);        break;  
+      case LIT_CHAR:            printf("LIT_CHAR '%d'\n", token);           break;  
+      case LIT_STRING:          printf("LIT_STRING '%d'\n", token);         break;  
+
+      case TOKEN_ERROR:         printf("TOKEN_ERROR '%d'\n", token);        break;  
+      default:                  printf("token '%d'\n", token);              break; 
+
+    }
+    token = yylex();  
+  }
+  printf("\n ---------- \n Numero de linhas = [%d] \n ---------- \n", getLineCount());    
+  hashPrint();
+}
